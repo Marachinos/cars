2493f6605ae63a57768b4391033c66f85b2068ea
Marachinos <stallnysater@gmail.com>
2025-09-19 07:21:40 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car2
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Color;
+        public int MaxSpeed;
+        public int HorsePower;
+        public CarType CarType;
+
+        
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Carinfo: {Brand}, {Color}, {MaxSpeed}");
+        }
+        public void Drive()
+        {
+            Console.WriteLine($"{Brand} is driving.");
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk, Honk!");
+        }
+
+        public void Fuel()
+        {
+            Console.WriteLine($"Car is fueled with {CarType}\n");
+
+            switch (CarType)
+            {
+                case CarType.Electric:
+                    Console.WriteLine();
+                    break;
+                case CarType.Gas:
+                    Console.WriteLine();
+                    break;
+                case CarType.Diesel:
+                    Console.WriteLine();
+                    break;
+                case CarType.Hybrid:
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+            public void IsFasterThen(Car otherCar)
+        {
+            if (otherCar.MaxSpeed > MaxSpeed)
+            {
+                Console.WriteLine($"Car {otherCar.Brand} is faster\n");
+            }
+            else if (otherCar.MaxSpeed == MaxSpeed)
+            {
+                Console.WriteLine($"They are equel");
+            }
+            else
+            {
+                Console.WriteLine($"Car {Brand} is quiest.");
+            }
+        }
+
+    }
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace car2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car ferrari = new Car();
+            ferrari.Color = "Red";
+            ferrari.Brand = "Ferrari";
+            ferrari.MaxSpeed = 350;
+            ferrari.HorsePower = 500;
+            ferrari.CarType = CarType.Gas;
+
+            Car volvo = new Car();
+            volvo.Color = "White";
+            volvo.Brand = "Volvo";
+            volvo.MaxSpeed = 200;
+            volvo.HorsePower = 300;
+            volvo.CarType = CarType.Diesel;
+
+            ferrari.ShowInfo();
+            ferrari.Drive();
+            ferrari.Honk();
+            ferrari.Fuel();
+
+            volvo.ShowInfo();
+            volvo.Drive();
+            volvo.Honk();
+            volvo.Fuel();
+
+            volvo.IsFasterThen(ferrari);
+        }
+    }
+}
